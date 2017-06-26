3dd1670c7d78f8b814ad662b1f25b1be1758f005
josephatj <josephatjulius24@gmail.com>
2017-06-26 12:03:52 +0300

    New app for web profile management

---
+++ app.js
@@ -0,0 +1,35 @@
+var express = require('express');
+
+var port = 3015;
+
+var app = express.createServer();
+
+function checkAuth (req, res, next) {
+	console.log('checkAuth ' + req.url);
+
+	// don't serve /secure to those not logged in
+	// you should add to this list, for each and every secure url
+	if (req.url === '/secure' && (!req.session || !req.session.authenticated)) {
+		res.render('unauthorised', { status: 403 });
+		return;
+	}
+
+	next();
+}
+
+app.configure(function () {
+
+	app.use(express.cookieParser());
+	app.use(express.session({ secret: 'example' }));
+	app.use(express.bodyParser());
+	app.use(checkAuth);
+	app.use(app.router);
+	app.set('view engine', 'jade');
+	app.set('view options', { layout: false });
+
+});
+
+require('./lib/routes.js')(app);
+
+app.listen(port);
+console.log('Node listening on port %s', port);
